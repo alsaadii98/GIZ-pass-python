127378543e4e8795a14556c7607b720f8221fe69
Ali H.Alsaadi <a.h.alsaady1998@gmail.com>
2021-09-30 17:15:48 +0300

    Ali H.Alsaadi Task 2 done

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,19 @@
-
-
 class Solution:
 
     @staticmethod
     def longest_palindromic(s: str) -> str:
         pass
+        def expandAroundCenter (s, left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -=1
+                right +=1
+            return s[left+1:right]
+        longestSub = ""
+        for i in range(len(s)):
+            center = expandAroundCenter(s, i, i)
+            inBetween = expandAroundCenter(s, i, i+1)
+            longestSub = max(longestSub, center, inBetween, key=len)
+        return print("Output : ", longestSub)
+
+s =input("input : s =  ")
+Solution.longest_palindromic(s)
